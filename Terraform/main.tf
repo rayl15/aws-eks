d9c2dbc1741692462dd78b005e30c5bdb06e80b0
Rahul Sharma <er.csrahul@gmail.com>
2024-08-09 23:29:15 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "TeKanAid"
+    organization = "rayl15"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "backstage-eks-aws"
+      name = "sourcefuse"
     }
   }
 }
