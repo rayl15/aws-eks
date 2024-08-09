25bc144ae859405a9bd21da87b21ad47a5a8303b
Rahul Sharma <er.csrahul@gmail.com>
2024-08-10 00:08:56 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+  value       = module.eks_al2.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+  value       = module.eks_al2.cluster_security_group_id
 }
 
 output "region" {
@@ -15,5 +15,5 @@ output "region" {
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
+  value       = module.eks_al2.cluster_name
 }
